e8dafd4db643ae2d6df5c98f096062839cdcfe49
Mahmoud-Moghazy <ma.moghazy@gmail.com>
2024-10-02 02:55:34 +0300

    add cairo as default time zone

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,15 +4,22 @@ import UpcomingCalls from '@/components/UpcomingCalls';
 const Home = () => {
   const now = new Date();
 
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now); 
+  const time = now.toLocaleTimeString('en-US', {
+    hour: '2-digit',
+    minute: '2-digit',
+    timeZone: 'Africa/Cairo'
+  });
+  const date = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'full',
+    timeZone: 'Africa/Cairo'
+  }).format(now);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <div className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-          <UpcomingCalls/>
+            <UpcomingCalls />
           </div>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
